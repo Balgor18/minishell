05e6acd3708e8774101d76602560c923226a81fd
Florian CATINAUD <fcatinau@e2r9p16.clusters.42paris.fr>
2022-01-06 06:05:56 +0100

    Add function in utils ft_strlcpy ft_strtrim ft_joinstr_from_tab

--- srcs/utils/utils2.c
+++ srcs/utils/utils2.c
@@ -6,7 +6,7 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 15:47:24 by elaachac          #+#    #+#             */
-/*   Updated: 2022/01/04 18:51:41 by fcatinau         ###   ########.fr       */
+/*   Updated: 2022/01/06 05:46:05 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,18 +37,72 @@ bool	ft_strchr(const char *s, int c)
 	return (false);
 }
 
-// Why check only 4 char --> check for only $L --> and check for $COUCOU
-
-char	*get_env_var(char **envp, char *to_find)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	**env;
+	size_t		size;
+	char		*s2;
 
-	env = envp;
-	while (*env != 0)
+	if (!s1 || !set)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	size = ft_strlen((char *)s1);
+	while (size && ft_strchr(set, s1[size - 1]))
+		size--;
+	s2 = ft_substr((char *)s1, 0, size);
+	return (s2);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t		i;
+
+	i = 0;
+	if (size > 0)
 	{
-		if (ft_strncmp(*env, to_find, 4) == 0)
-			return (*env);
-		env++;
+		while (src[i] && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	return (NULL);
+	while (src[i])
+		i++;
+	return (i);
+}
+
+//return NULL if error
+//size[0] == LEN MAX OF EVERY *TAB
+//size[1] == Get the return elem of ft_strlcpy
+char	*ft_joinstr_from_tab(char **tab)
+{
+	char	*ret;
+	size_t	size[2];//<--check for size of 2;
+	char	**start;
+
+	start = tab;
+	size[0] = 0;
+	size[1] = 0;
+	while (*tab)
+	{
+		if (*(*tab))
+			size[0] += ft_strlen(*tab);
+		tab++;
+	}
+	if (size[0] == 0)
+		return (NULL);
+	ret = malloc(sizeof(char *) * (size[0] + 1));
+	if (!ret)
+		return (NULL);
+	tab = start;
+	while (*start)
+	{
+		if (*(*start))
+			size[1] += ft_strlcpy(ret + size[1], *start, ft_strlen(*start) + 1);
+		free(*start);
+		start++;
+	}
+	free(tab);
+	return (ret);
 }
