34f7be9712d03de10d0ac00ebfa5e7fd26fa85ca
Florian CATINAUD <fcatinau@e2r9p16.clusters.42paris.fr>
2022-02-28 10:31:26 +0100

    segfault found

--- srcs/builtins/builtins_echo.c
+++ srcs/builtins/builtins_echo.c
@@ -6,7 +6,7 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 11:46:23 by fcatinau          #+#    #+#             */
-/*   Updated: 2022/02/06 11:27:31 by fcatinau         ###   ########.fr       */
+/*   Updated: 2022/02/28 10:30:53 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int	builtins_echo(t_node *arg, int fd_out)
 	endl = true;
 	if (!arg || !arg->word)
 		return (write(fd_out, "\n", 1), true);
-	while (verif_flag_n(arg->word))
+	while (arg && verif_flag_n(arg->word))
 	{
 		endl = false;
 		arg = arg->next;
