787fdde123a4efd9da830ec1fedd6a85c74da142
Florian CATINAUD <fcatinau@e2r9p16.clusters.42paris.fr>
2022-03-17 23:42:27 +0100

    modif en builtins echo

--- srcs/builtins/builtins_echo.c
+++ srcs/builtins/builtins_echo.c
@@ -6,7 +6,7 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 11:46:23 by fcatinau          #+#    #+#             */
-/*   Updated: 2022/03/11 17:43:40 by fcatinau         ###   ########.fr       */
+/*   Updated: 2022/03/17 23:31:18 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ int	builtins_echo(t_node *arg, int fd_out)
 	int	endl;
 
 	endl = true;
-	if (!arg && !arg->word && !arg->next)
+	if (!arg )
 		return (write(fd_out, "\n", 1), true);
 	while (arg->word && verif_flag_n(arg->word))
 	{
